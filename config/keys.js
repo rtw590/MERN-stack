b12bef5ccf7682da4c5048d86be73c5818b909e8
Rob Waller <r.t.waller@gmail.com>
2018-04-21 10:15:26 -0500

    Started validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://rob:qazqaz11@ds139801.mlab.com:39801/devconnector"
+  mongoURI: "mongodb://rob:qazqaz11@ds139801.mlab.com:39801/devconnector",
+  secretOrKey: "secret"
 };
